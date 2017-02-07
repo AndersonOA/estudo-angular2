a7785b19ad72437d13e752b411f0ce6ca07fcc6f
Anderson Orione Aristides <marrecogevans@gmail.com>
2017-02-07 13:25:17 -0200

    aula07 template

---
+++ aula07-template/app/app-component.ts
@@ -0,0 +1,46 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'meu-app',
+  templateUrl: 'app/views/app.html'
+})
+export class AppComponent {
+	// ATRIBUTOS
+    // interpolação
+    
+
+    // atribuições
+    
+
+    // one way e two way binding
+    
+
+    // css class
+    
+
+    // *ngIf
+    
+
+    // *ngFor
+    
+
+    // MÉTODOS
+    // interpolação
+    
+
+    // eventos
+    
+
+
+    // atribuições
+    
+
+    // one way e two way binding
+    
+
+    // css class
+    
+
+    // *ngIf
+    
+}
