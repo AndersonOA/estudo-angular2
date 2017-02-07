e35b098e564b0653d08f2f96b144479a2915bf28
Anderson Orione Aristides <marrecogevans@gmail.com>
2017-02-07 14:52:17 -0200

    aaa

--- aula07-template/app/app-component.ts
+++ aula07-template/app/app-component.ts
@@ -7,13 +7,33 @@ import {Component} from 'angular2/core';
 export class AppComponent {
 	// ATRIBUTOS
     // interpolação
-    
+    public olaFulano: string = 'Olá, Fulano';
+    public mostrarMensagem: boolean = true;
+    public nome: string = 'Fulano';
+    public nome2: string = 'Ciclano';
+
+    obterJuros(): number {
+        return 100;
+    }
 
     // atribuições
+    dizerOi(): void {
+        alert(this.olaFulano);
+    }
+
+    mostrarTexto($event): void {
+        alert($event.target.value);
+    }
+
+    mudarMostrarMensagem(): void {
+        this.mostrarMensagem = !this.mostrarMensagem;
+    }
     
 
     // one way e two way binding
-    
+    atualizarNome($event): void {
+        this.nome = $event.target.value;
+    }
 
     // css class
     
